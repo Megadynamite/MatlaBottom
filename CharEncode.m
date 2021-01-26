38a663412dcfe360a1599f6bbc06d3867c317157
Megadynamite <50307653+Megadynamite@users.noreply.github.com>
2021-01-26 00:40:28 -0700

    Implement Character Encoding

--- CharEncode.m
+++ CharEncode.m
@@ -1,1 +1,22 @@
-
+function [encoded] = CharEncode(rem, testlist)
+    disp(rem)
+    disp(testlist)
+    for iter = 1:length(testlist)
+        testlist(3,iter) = floor(rem/double(testlist(1,iter)))
+        rem = mod(rem, double(testlist(1,iter)))
+    end
+    help = testlist(3,:);
+    help(isnan(double(help)))=0;
+    testlist(3,:)=help;
+    for iter = 1:length(testlist)
+        if double(testlist(3,iter)) ~= 0
+        testlist(4,iter) = strjoin(repmat(testlist(2,iter),[1,double(testlist(3,iter))]), '');
+        end
+    end
+    disp(testlist)
+    disp(help)
+    disp(iter)
+    encoded = strjoin(testlist(4,:), '');
+    disp(encoded)
+    end
+    
