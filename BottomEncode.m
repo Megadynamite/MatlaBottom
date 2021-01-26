9bee31ba03a17f32fe1e3122e1499a3dd363547d
Megadynamite <50307653+Megadynamite@users.noreply.github.com>
2021-01-26 01:12:23 -0700

    Encoding V1 Stable

--- BottomEncode.m
+++ BottomEncode.m
@@ -1,1 +1,9 @@
-
+function [encoded] = BottomEncode(input)
+    testlist = [200, 50, 10, 5, 1, 0; ":people_hugging:", ":sparkling_heart:", ":sparkles:", ":pleading_face:", ",", ":heart:"; NaN, NaN, NaN, NaN, NaN, NaN; "", "", "", "", "", ""];
+    disp(unicode2native(input, 'UTF-8'))
+    encodedList = []
+    for iter = unicode2native(input, 'UTF-8')
+        encodedList = [encodedList,CharEncode(double(iter), testlist)]
+    end
+    encoded = strcat(strjoin(encodedList,':point_right::point_left:'), ':point_right::point_left:');
+end
